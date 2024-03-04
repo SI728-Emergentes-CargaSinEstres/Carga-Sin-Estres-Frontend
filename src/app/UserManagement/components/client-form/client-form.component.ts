c02b669bbcb60db55f6d1adf51d53458c2b1d48d
alanegd <alangalavisd@gmail.com>
2024-03-04 11:27:49 -0500

    chore: Added birthDate to customer signup form

--- src/app/UserManagement/components/client-form/client-form.component.ts
+++ src/app/UserManagement/components/client-form/client-form.component.ts
@@ -17,6 +17,7 @@ export class ClientFormComponent {
     this.clientRegistrationForm = this.fb.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
       phoneNumber: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*\d).{12,}$/)]],
@@ -58,7 +59,7 @@ export class ClientFormComponent {
         phoneNumber: formData.phoneNumber,
         email: formData.email,
         password: formData.password,
-        userType: 'client',
+        dateOfBirth: formData.dateOfBirth,
       }
 
       this.api.createClient(clientData).subscribe((clientResponse: any) => {
@@ -72,9 +73,5 @@ export class ClientFormComponent {
 
     }
 
-  }
-
-  cancelar(){
-    this.router.navigate(['landing-page']);
   }
 }
